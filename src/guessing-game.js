e45fbab63a5a68d68620e599de8e897b1b44a07a
wlukla <vladislav.danilochkin@gmail.com>
2019-09-25 00:04:14 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.arr = [];
+        this.mid = 0;
+        for (min; min <= max; min++) {
+            this.arr.push(min);
+        }
     }
 
     guess() {
-
+        this.mid = this.arr[Math.round((this.arr.length - 1) / 2)];
+        return this.mid;
     }
 
     lower() {
-
+        this.setRange(this.min, this.mid);
     }
 
     greater() {
-
+        this.setRange(this.mid, this.max);
     }
-}
+  }
 
-module.exports = GuessingGame;
+  module.exports = GuessingGame;
